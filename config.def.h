2ed08028e44d922a884e75fc8eb4e216adb05a97
dat-nix <dp2838400@gmail.com>
2026-02-24 22:15:32 +0700

    glyps support

--- config.def.h
+++ config.def.h
@@ -5,12 +5,14 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:size=11:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:style=Regular:size=13:antialias=true:hinting=true";
 static int borderpx = 2;
 /* Spare fonts */
 static char *font2[] = {
-  "Noto Color Emoji:size=12",
-  "Symbols Nerd Font:size=12"
+   "JetBrainsMono Nerd Font:style=Bold:size=10",
+   "JetBrainsMono Nerd Font:style=Italic:size=10",
+   "Noto Color Emoji:size=10",
+   "Noto Sans CJK JP:size=10"
 };
 
 /*
@@ -71,6 +73,18 @@ static unsigned int blinktimeout = 800;
  * thickness of underline and bar cursors
  */
 static unsigned int cursorthickness = 2;
+
+/*
+ * 1: render most of the lines/blocks characters without using the font for
+ *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
+ *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
+ * 0: disable (render all U25XX glyphs normally from the font).
+ */
+const int boxdraw = 0;
+const int boxdraw_bold = 0;
+
+/* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
+const int boxdraw_braille = 0;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
